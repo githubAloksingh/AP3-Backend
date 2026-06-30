1af2a419d654e30c59e9dc8f38332a22b097508a
Alok Singh <alok.v.singh@newgensoft.com>
2026-06-30 10:59:32 +0530

    Add common response and exception handling

---
+++ src/main/java/com/loanmanagement/common/constant/AppConstants.java
@@ -0,0 +1,11 @@
+package com.loanmanagement.common.constant;
+
+public final class AppConstants {
+
+    private AppConstants() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    public static final String SUCCESS = "Success";
+    public static final String FAILED = "Failed";
+}
