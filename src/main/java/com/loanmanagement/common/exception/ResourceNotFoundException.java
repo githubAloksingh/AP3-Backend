b6da623b9871bc8093bb6e7eff8d805bf0d171c5
Alok Singh <alok.v.singh@newgensoft.com>
2026-06-30 12:49:34 +0530

    Remove advanced project structure

--- src/main/java/com/loanmanagement/common/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.loanmanagement.common.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
