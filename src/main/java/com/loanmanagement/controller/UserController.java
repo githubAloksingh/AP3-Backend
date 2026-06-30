148d168534de3c9d366c940fbac4cd5a334e8c1e
Alok Singh <alok.v.singh@newgensoft.com>
2026-06-30 13:18:59 +0530

    Create UserController and basic User APIs

---
+++ src/main/java/com/loanmanagement/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.loanmanagement.controller;
+
+import com.loanmanagement.entity.User;
+import com.loanmanagement.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return "User deleted successfully";
+    }
+}
